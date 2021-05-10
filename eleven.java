2e3cabe79320498e34a509eb62f8647bde40dd04
jyothika-22 <72615323+jyothika-22@users.noreply.github.com>
2021-05-10 11:12:37 +0530

    Add files via upload

---
+++ eleven.java
@@ -0,0 +1,13 @@
+package Wpn;
+
+public class eleven {
+
+	public static void main(String[] args) {
+		for(int i =23;i<=57;i++) {
+			if((i&1)!=1) {
+				System.out.println(i+" ");
+			}
+		}
+	}
+
+}
