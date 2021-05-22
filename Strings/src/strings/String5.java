bc71d51176c0aec3ed5a1aeb9072e2ca2b89eef9
RAGHAVULU <RAGHAVULU@DESKTOP-IBFEQP0>
2021-05-22 13:10:03 +0530

    Strings

---
+++ Strings/src/strings/String5.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class String5 {
+
+
+		public static void main(String[] args) {
+			String str = "Suman";
+			
+			str = str.substring(1, str.length() - 1);
+			
+			System.out.println(str);
+		}
+
+	}
+
---
+++ Strings/src/strings/String6.java
@@ -0,0 +1,19 @@
+package strings;
+
+
+public class String6 {
+
+	public static void main(String[] args) {
+		String a = "hi";
+		String b = "hello";
+		String output;
+
+		if (a.length() < b.length()) 
+			output = a + b + a;
+		else
+			output = b + a + b;
+		
+		System.out.println(output);
+	}
+
+}
