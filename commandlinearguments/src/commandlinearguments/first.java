64361b699787ff01ad283125a6628850c5e2198b
RAGHAVULU <RAGHAVULU@DESKTOP-IBFEQP0>
2021-05-22 13:08:25 +0530

    CommandLineArguments

---
+++ commandlinearguments/src/commandlinearguments/first.java
@@ -0,0 +1,12 @@
+package commandlinearguments;
+
+public class first {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(args[0]+" "+"Technologies"+" "+args[1]);
+
+	}
+
+}
---
+++ commandlinearguments/src/commandlinearguments/second.java
@@ -0,0 +1,10 @@
+package commandlinearguments;
+
+public class second {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome"+" "+args[0]);
+
+	}
+
+}
